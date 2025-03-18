2d837e9431462563d064255fde67cba789b978bb
MendioX <mendiolacolombonahueltm6@gmail.com>
2025-03-18 13:50:18 -0300

    fix domains

--- server-mail.js
+++ server-mail.js
@@ -14,7 +14,8 @@ const whitelist = process.env.WHITELIST.split(",");
 const validateOrigin = (req, res, next) => {
   const allowedDomains = [
       "vps-4768993-x.dattaweb.com",
-      "http://localhost:3000"
+      "http://localhost:3000",
+      "http://127.0.0.1:5000"
     ]; // Reemplaza con tu dominio
   
   
