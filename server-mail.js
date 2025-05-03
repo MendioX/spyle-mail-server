8e7b25ccb4c9a9920fc3a2bfb26fbd0780f95dd9
MendioX <mendiolarcolombonahuelmt6@gmail.com>
2025-05-03 10:43:26 -0300

    add domain

--- server-mail.js
+++ server-mail.js
@@ -14,6 +14,7 @@ const whitelist = process.env.WHITELIST.split(",");
 const validateOrigin = (req, res, next) => {
   const allowedDomains = [
       "https://vps-4768993-x.dattaweb.com",
+      "https://spyle.com.ar",
       "http://localhost:5000",
       "http://127.0.0.1:5000"
     ]; // Reemplaza con tu dominio
